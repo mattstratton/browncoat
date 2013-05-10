45e03776c11a326a7f91938016823d8dc90c7819
Matt Stratton <matt.stratton@gmail.com>
2013-05-10 13:41:31 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Browncoat::Application.config.secret_token = 'f1cde25e216416b7e7df6586436fc113ccef0017f94ac01e5be7c70be6fdde03444da9381603fcfd0068a6041be2590fa495fb44e86677799dbb0d3e604b0049'
